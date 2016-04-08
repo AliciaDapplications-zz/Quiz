ec7242b94c665814f9ef1654f4995b2c5166f357
AliciaDapplications <boricua21dale@gmail.com>
2016-04-07 19:34:54 -0500

    Initial Commit

---
+++ QUIZZ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QUIZZ
+//
+//  Created by Alicia Dale on 4/7/16.
+//  Copyright © 2016 Alicia Dale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
