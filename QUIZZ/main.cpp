94127f1685b0c5edd3cd7c0e307c33c61aa270eb
AliciaDApplications <boricua21dale@gmail.com>
2017-03-17 19:37:43 -0500

    New Quiz

--- QUIZZ/main.cpp
+++ QUIZZ/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // DECLARE THE VARIALBES TO BE USED IN THE PROJECT
+    string answer;
+    
+        //DISPLAY THE QUESTION 1 ON THE CONSOLE
+    cout << "Q1-Every Statement in C++ language must terminate with: \n";
+    cout << "\t a. Semi Colon ; ";
+    cout << "\t b. Comma , ";
+    cout << "\t c. Period . ";
+    cout << "\t d. Question Mark ? ";
+    cout << endl;
+    cout << endl;
+    
+        //PROMPT THE USER TO ENTER THE LETTER (A, B, C, D) FOR CORRECT ANSWER
+    cout << "Enter the letter (a, b, c, d) for correct Answer: ";
+    
+        //READ THE USER ANSWER FROM THE CONSOLE
+    cin >> answer;
+    
+        //CHECK IF THE ANSWER IS CORRECT ANSWER 'A'
+    if (answer == "a" || answer == "A")
+    {
+        cout << "Your answer " << answer << " is Correct.";
+    }
+    else
+    {
+        cout << "Your answer " << answer << " is Wrong.";
+        
+    }
+    cout << endl;
+    cout << "**********************\n\n";
+    
+            return 0;
 }
